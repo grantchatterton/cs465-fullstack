f2ba23cec228966eb0727272afda2e660a19139f
Grant Chatterton <grantchatterton@gmail.com>
2024-02-09 07:23:59 -0600

    Updated to fetch trips from the API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,13 +1,53 @@
-const Trip = require('../models/travlr');
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
+
+/* Internal method to render the travel list */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = 'Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+
+    res.render('travel',
+        {
+            title: pageTitle,
+            layout: 'layouts/layout',
+            trips: responseBody,
+            message
+        }
+    );
+};
 
 /* GET travel view */
-const travel = (req, res) => {
-    Trip.find().exec()
-        .then((trips) => {
-            res.render('travel', { title: 'Travel', layout: 'layouts/layout', trips });
-        });
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {}
+    };
+
+    console.info(`>> travelController.travelList calling ${requestOptions.url}`);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            
+            renderTravelList(req, res, body);
+        }
+    );
 };
 
-module.exports = {
-    travel
-};
+module.exports = travelList;
