86cfac561e75a64f5bec7b8a3606f504e06374f1
Grant Chatterton <grantchatterton@gmail.com>
2024-02-16 09:25:19 -0600

    Modify to allow Cross-Origin Resource Sharing (CORS) from the Angular admin app

--- app.js
+++ app.js
@@ -1,63 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var hbs = require('hbs');
-require('./app_api/models/db');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var hbs = require("hbs");
+require("./app_api/models/db");
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-const travelRouter = require('./app_server/routes/travel');
-const roomsRouter = require('./app_server/routes/rooms');
-const mealsRouter = require('./app_server/routes/meals');
-const newsRouter = require('./app_server/routes/news');
-const aboutRouter = require('./app_server/routes/about');
-const contactRouter = require('./app_server/routes/contact');
-const apiRouter = require('./app_api/routes/index');
+var indexRouter = require("./app_server/routes/index");
+var usersRouter = require("./app_server/routes/users");
+const travelRouter = require("./app_server/routes/travel");
+const roomsRouter = require("./app_server/routes/rooms");
+const mealsRouter = require("./app_server/routes/meals");
+const newsRouter = require("./app_server/routes/news");
+const aboutRouter = require("./app_server/routes/about");
+const contactRouter = require("./app_server/routes/contact");
+const apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
-// if true, populates the db w/ starter trip data
-const popStartTrips = false;
-
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "app_server", "views"));
+app.set("view engine", "hbs");
 
 // register handlebars partials
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
-app.use('/rooms', roomsRouter);
-app.use('/meals', mealsRouter);
-app.use('/news', newsRouter);
-app.use('/about', aboutRouter);
-app.use('/contact', contactRouter);
-app.use('/api', apiRouter);
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/travel", travelRouter);
+app.use("/rooms", roomsRouter);
+app.use("/meals", mealsRouter);
+app.use("/news", newsRouter);
+app.use("/about", aboutRouter);
+app.use("/contact", contactRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
