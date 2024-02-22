3270d83e2de149349ae12b9a2e1192402c8c55b4
Grant Chatterton <grantchatterton@gmail.com>
2024-02-22 07:39:03 -0600

    Make edit and delete buttons available if logged in

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { Trip } from '../models/trip';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { Trip } from "../models/trip";
+import { AuthenticationService } from "../services/authentication.service";
 
 @Component({
-  selector: 'app-trip-card',
-  templateUrl: './trip-card.component.html',
-  styleUrls: ['./trip-card.component.css']
+  selector: "app-trip-card",
+  templateUrl: "./trip-card.component.html",
+  styleUrls: ["./trip-card.component.css"],
 })
 export class TripCardComponent implements OnInit {
-
-  @Input('trip') trip: any;
+  @Input("trip") trip: any;
   allowDelete = false;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
   }
 
   private updateTripCodeAndNavigate(trip: Trip, route: string): void {
@@ -26,10 +30,10 @@ export class TripCardComponent implements OnInit {
   }
 
   private editTrip(trip: Trip): void {
-    this.updateTripCodeAndNavigate(trip, 'edit-trip');
+    this.updateTripCodeAndNavigate(trip, "edit-trip");
   }
 
   private deleteTrip(trip: Trip): void {
-    this.updateTripCodeAndNavigate(trip, 'delete-trip');
+    this.updateTripCodeAndNavigate(trip, "delete-trip");
   }
 }
