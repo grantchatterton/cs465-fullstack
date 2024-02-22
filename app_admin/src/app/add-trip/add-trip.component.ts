ee4ace684d0ed2134d3baed27ac84ed5a3a0ab33
Grant Chatterton <grantchatterton@gmail.com>
2024-02-22 07:34:31 -0600

    Post trip modification now redirects to trip list

--- app_admin/src/app/add-trip/add-trip.component.ts
+++ app_admin/src/app/add-trip/add-trip.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { TripDataService } from '../services/trip-data.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { TripDataService } from "../services/trip-data.service";
 
 @Component({
-  selector: 'app-add-trip',
-  templateUrl: './add-trip.component.html',
-  styleUrls: ['./add-trip.component.css']
+  selector: "app-add-trip",
+  templateUrl: "./add-trip.component.html",
+  styleUrls: ["./add-trip.component.css"],
 })
 export class AddTripComponent implements OnInit {
-
   addForm: FormGroup;
   submitted = false;
 
@@ -17,33 +16,34 @@ export class AddTripComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private tripService: TripDataService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.addForm = this.formBuilder.group({
-        _id: [],
-        code: ['', Validators.required],
-        name: ['', Validators.required],
-        length: ['', Validators.required],
-        start: ['', Validators.required],
-        resort: ['', Validators.required],
-        perPerson: ['', Validators.required],
-        image: ['', Validators.required],
-        description: ['', Validators.required],
+      _id: [],
+      code: ["", Validators.required],
+      name: ["", Validators.required],
+      length: ["", Validators.required],
+      start: ["", Validators.required],
+      resort: ["", Validators.required],
+      perPerson: ["", Validators.required],
+      image: ["", Validators.required],
+      description: ["", Validators.required],
     });
   }
 
   onSubmit() {
     this.submitted = true;
     if (this.addForm.valid) {
-      this.tripService.addTrip(this.addForm.value)
-        .then(data => {
-          console.log(data);
-          this.router.navigate(['']);
-        });
+      this.tripService.addTrip(this.addForm.value).then((data) => {
+        console.log(data);
+        this.router.navigate(["/list-trips"]);
+      });
     }
   }
 
   // get the form short name to access the form fields
-  get f() { return this.addForm.controls; }
+  get f() {
+    return this.addForm.controls;
+  }
 }
--- app_admin/src/app/delete-trip/delete-trip.component.ts
+++ app_admin/src/app/delete-trip/delete-trip.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { TripDataService } from '../services/trip-data.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { TripDataService } from "../services/trip-data.service";
 
 @Component({
-  selector: 'app-delete-trip',
-  templateUrl: './delete-trip.component.html',
-  styleUrls: ['./delete-trip.component.css']
+  selector: "app-delete-trip",
+  templateUrl: "./delete-trip.component.html",
+  styleUrls: ["./delete-trip.component.css"],
 })
 export class DeleteTripComponent implements OnInit {
-
-  constructor(
-    private router: Router,
-    private service: TripDataService
-  ) { }
+  constructor(private router: Router, private service: TripDataService) {}
 
   ngOnInit() {
     // retrieve stashed tripId
     const tripCode = localStorage.getItem("tripCode");
     if (!tripCode) {
       alert("Something wrong, couldn't find where I stashed tripCode!");
-      this.router.navigate(['']);
+      this.router.navigate(["/list-trips"]);
       return;
     }
 
-    this.service.deleteTrip(tripCode)
-      .then(data => {
-        console.log(data);
-        this.router.navigate(['']);
-      });
+    this.service.deleteTrip(tripCode).then((data) => {
+      console.log(data);
+      this.router.navigate(["/list-trips"]);
+    });
   }
 }
